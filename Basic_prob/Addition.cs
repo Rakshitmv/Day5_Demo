95e903be36c685bbe4f9f8cab75a484e195c2d5e
Rakshitmv <rakshit8976646711@gmail.com>
2022-01-30 10:11:07 +0530

    [Rakshit] Add: program

---
+++ Basic_prob/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_prob
+{
+    public class Addition
+    {
+        public void Add()
+        {
+            Console.WriteLine("Helllo");
+            Console.ReadLine();
+        }
+    }
+}
